5e37b8e89b08fb0ebabb0ecff807405260658bc3
Ellie Carpenter <espanswick@gmail.com>
2024-11-28 17:00:53 +0000

    Recreate Project Model

--- projects/models.py
+++ projects/models.py
@@ -7,27 +7,27 @@ STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
 
 
-# class Projects(models.Model):
-#     """
-#     Stores a single project post entry reated to :model:`auth.User`.
-#     """
-#     title = models.CharField(max_length=200, unique=True)
-#     slug = models.SlugField(max_length=200, unique=True)
-#     author = models.ForeignKey(
-#         User, on_delete=models.CASCADE, related_name="project_posts"
-#     )
-#     # featured_image = CloudinaryField('image', default='placeholder')
-#     content = models.TextField()
-#     created_on = models.DateTimeField(auto_now_add=True)
-#     status = models.IntegerField(choices=STATUS, default=0)
-#     excerpt = models.TextField(blank=True)
-#     updated_on = models.DateTimeField(auto_now=True)
+class Project(models.Model):
+    """
+    Stores a single project post entry reated to :model:`auth.User`.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="project_posts"
+    )
+    # featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
-#     class Meta:
-#         ordering = ["-created_on"]
+    class Meta:
+        ordering = ["-created_on"]
 
-#     def __str__(self):
-#         return f"{self.title} | written by {self.author}"
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 
 # class Comment(models.Model):
