2a4547a929ec4fe437de3412c54a9694e2dac3ac
Ellie Carpenter <espanswick@gmail.com>
2024-11-29 15:19:21 +0000

    Add pagination, previous button and create post detail view function for projects

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
-from . import views
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
     path('', views.ProjectList.as_view(), name='home'),
-]
+    path('<slug:slug>/', views.project_detail, name="project_detail"),
+    ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
-from .models import Project
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Project, Comment
+# from .forms import CommentForm
 
 # Create your views here.
 
@@ -8,3 +11,46 @@ class ProjectList(generic.ListView):
     queryset = Project.objects.filter(status=1)
     template_name = "projects/index.html"
     paginate_by = 6
+    
+
+def project_detail(request, slug):
+    """
+    Display an individual :model:projects.Project`.
+
+    **Context**
+
+    ``project``
+        An instance of :model:`projects.Project`.
+
+    **Template:**
+
+    :template:`projects/project_detail.html`
+    """
+    queryset = Project.objects.filter(status=1)
+    project = get_object_or_404(queryset, slug=slug)
+    comments = project.comments.all().order_by("-created_on")
+    comment_count = project.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.project = project
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    
+    comment_form = CommentForm()
+
+    return render(
+        request,
+        "projects/project_detail",
+        {
+            "project": project,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
+        },
+    )
