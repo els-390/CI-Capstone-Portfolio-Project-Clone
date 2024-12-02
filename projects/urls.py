d9f181e4d04ed63af93c0884df3ac9a517bf90be
Ellie Carpenter <espanswick@gmail.com>
2024-12-02 16:46:29 +0000

    Update project views and urls with comment edit function

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.ProjectList.as_view(), name='home'),
     path('<slug:slug>/', views.project_detail, name="project_detail"),
-    ]
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
+]
