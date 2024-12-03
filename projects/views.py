b488d99fc273d5076078dc590f95be99dde425a9
Ellie Carpenter <espanswick@gmail.com>
2024-12-03 16:52:46 +0000

    Fix missing POST in project_detail.html breaking comment function

--- projects/views.py
+++ projects/views.py
@@ -56,6 +56,7 @@ def project_detail(request, slug):
                 request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
             )
+        
     
     comment_form = CommentForm()
 
