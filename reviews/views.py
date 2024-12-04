916bbfba186f039541fbc2e68f22b3b514c31a16
Ellie Carpenter <espanswick@gmail.com>
2024-12-04 16:25:16 +0000

    Update add_review, review_list styling and add edit review function to views.py

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,8 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .models import Review
 from .forms import ReviewForm
 
@@ -37,59 +39,15 @@ def add_review(request):
 
     return render(request, 'reviews/add_review.html', {'form': form})
 
-# def comment_edit(request, slug, comment_id):
-#     """
-#     Display an individual comment for edit.
-
-#     **Context**
-
-#     ``project``
-#         An instance of :model:`projects.Project`.
-#     ``comment``
-#         A single comment related to the project.
-#     ``comment_form``
-#         An instance of :form:`projects.CommentForm`
-#     """
-#     if request.method == "POST":
-
-#         queryset = Project.objects.filter(status=1)
-#         project = get_object_or_404(queryset, slug=slug)
-#         comment = get_object_or_404(Comment, pk=comment_id)
-#         comment_form = CommentForm(data=request.POST, instance=comment)
-
-#         if comment_form.is_valid() and comment.author == request.user:
-#             comment = comment_form.save(commit=False)
-#             comment.project = project
-#             comment.approved = False
-#             comment.save()
-#             messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
-#         else:
-#             messages.add_message(request, messages.ERROR,
-#                                  'Error updating comment!')
-
-#     return HttpResponseRedirect(reverse('project_detail', args=[slug]))
-
-
-# def comment_delete(request, slug, comment_id):
-#     """
-#     Delete an individual comment.
-
-#     **Context**
-
-#     ``project``
-#         An instance of :model:`projects.Project`.
-#     ``comment``
-#         A single comment related to the project.
-#     """
-#     queryset = Project.objects.filter(status=1)
-#     project = get_object_or_404(queryset, slug=slug)
-#     comment = get_object_or_404(Comment, pk=comment_id)
-
-#     if comment.author == request.user:
-#         comment.delete()
-#         messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
-#     else:
-#         messages.add_message(request, messages.ERROR,
-#                              'You can only delete your own comments!')
-
-#     return HttpResponseRedirect(reverse('project_detail', args=[slug]))
+def edit_review(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Review Updated!')
+            return redirect('reviews_list')  # Replace with your reviews list view name
+        else:
+            form = ReviewForm(instance=review)
+            messages.add_message(request, messages.ERROR, 'Error updating review!')
+    return render(request, 'reviews/edit_review.html', {'form': form})
